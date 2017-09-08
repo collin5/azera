4e1fe8169e2bca186a6e51fa44c945cd9559d595
Seremba John <john.seremba@andela.com>
2017-09-08 22:47:36 +0300

    Add android play services to gradle.buyild

--- app/src/main/java/com/andela/hackathon/azera/LoginActivity.java
+++ app/src/main/java/com/andela/hackathon/azera/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.andela.hackathon.azera;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+			GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+					.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+					.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+					.build();
+
     }
 }
