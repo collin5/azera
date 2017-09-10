59e60b2d36608feb84bc9d7b71d582ba6c37bfa2
Collins Abitekaniza <abtcolns@gmail.com>
2017-09-10 20:20:46 +0300

    Fix casting string into long bug

--- app/src/main/java/com/andela/hackathon/azera/main/Receipt.java
+++ app/src/main/java/com/andela/hackathon/azera/main/Receipt.java
@@ -7,19 +7,22 @@ class Receipt {
 	public String status;
 	public String user_id;
 	public String description;
-	public Long updatedAt;
-	public Long createdAt;
+	public String updatedAt;
+	public String createdAt;
 
 	public Receipt() {
 
 	}
 
-	public Receipt(String category, String tags, String imgUrl, String status, String user_id, String description) {
+	public Receipt(String category, String tags, String imgUrl, String status, String user_id, String description, String updatedAt, String createdAt) {
 		this.category = category;
 		this.tags = tags;
 		this.imageUrl = imgUrl;
 		this.status = status;
 		this.user_id = user_id;
 		this.description = description;
+		this.updatedAt = updatedAt;
+		this.createdAt = createdAt;
+
 	}
 }
